c3b451d2bca6e5292a070d47217e2d3e33291ab2
Arthur Bit-Monnot <arthur.bit-monnot@laas.fr>
2019-02-13 14:27:55 +0100

    Add test for platform looking first in file system and then in resources.

--- core/tests/src/ham/PlatformTests.scala
+++ core/tests/src/ham/PlatformTests.scala
@@ -6,9 +6,15 @@ import minitest._
 
 object PlatformTests extends SimpleTestSuite {
 
-  test("load resource file") {
-    val platform = Platform.resources
+  test("load resource file (from resources)") {
+    testsResourceRead(Platform.resources)
+  }
+  test("laod resource file (from file system or else resources)") {
+    testsResourceRead(Platform.fileSystemOrElseResources)
+  }
 
+
+  def testsResourceRead(platform: Platform): Unit = {
     assertSucceedsTo(
       platform.readPath("content.txt"),
       "AA AA\nBB BB\n\nDD DD")
@@ -19,7 +25,6 @@ object PlatformTests extends SimpleTestSuite {
     )
 
     assertFails(platform.readPath("does-not-exists.txt"))
-
   }
 
 }
